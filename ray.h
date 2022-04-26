2145672ee23d4b261749f1d48b387d18e56b0cd8
H7perus <jonathanott.gd@gmail.com>
2022-04-26 23:09:54 +0200

    "functionizing" the subprocesses(e.g intersections).

--- ray.h
+++ ray.h
@@ -22,33 +22,31 @@ public:
 		direction = glm::reflect(direction, normal);
 	}
 
-	void rayTrace(sphere& sphere, png::image<png::rgb_pixel>& image, float FOV, png::image<png::rgb_pixel>& HDRI)
+	void rayTrace(sphere& sphere, png::image<png::rgb_pixel>& image, float FOV, png::image<png::rgb_pixel>& HDRI, png::image<png::rgb_pixel>& BaseTexture)
 	{
+		float BasePlaneHeight = -2.0f;
 
 		float FocalLength = (1 / tan(FOV / 360 * PI)) * (image.get_width() / 2);
 
-		ray ray;
-		ray.origin = glm::vec3(0.0f, 0.0f, 0.0f);
-
+		float BaseTexScale = 500;
 
 		for (png::uint_32 y = 0; y < image.get_height(); y++) {
 
 			for (png::uint_32 x = 0; x < image.get_width(); x++) {
 
-				ray.origin = glm::vec3(0, 0, 0);
+				origin = glm::vec3(0.0f, 0.0f, 0.0f);
 
 				glm::ivec3 brightness(0, 0, 0);
 				float z = FocalLength;
 				auto testvector = glm::vec3(float(x) - image.get_width() / 2 + 0.5f, float(y) - image.get_height() / 2 + 0.5f, float(z));
-				ray.direction = glm::normalize(testvector);
+				direction = glm::normalize(testvector);
 
 				glm::vec3 spherePositionNormalized = glm::normalize(sphere.pos);
-				float angle = glm::angle(ray.direction, spherePositionNormalized);
-				//std::cout << angle << std::endl;
+				float angle = glm::angle(direction, spherePositionNormalized);
+
 				float c = glm::sqrt(pow(sphere.pos.x, 2) + pow(sphere.pos.y, 2) + pow(sphere.pos.z, 2));
 				float sinValue = sin(angle);
 				float radiusCheck = sinValue * c;
-				//std::cout << angle << std::endl;
 
 
 				bool HDRIhit = false;
@@ -57,74 +55,142 @@ public:
 
 				while (!HDRIhit)
 				{
-					float angle = glm::angle(ray.direction, glm::normalize(sphere.pos - ray.origin));
-					//std::cout << glm::reflect(glm::vec3(0.0f, 1.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f)).x << glm::reflect(glm::vec3(0.0f, 1.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f)).y << glm::reflect(glm::vec3(0.0f, 1.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f)).z << std::endl;
-					float c = glm::sqrt(pow((sphere.pos - ray.origin).x, 2) + pow((sphere.pos - ray.origin).y, 2) + pow((sphere.pos - ray.origin).z, 2));
+					float angle = glm::angle(direction, glm::normalize(sphere.pos - origin));
+					float c = glm::sqrt(pow((sphere.pos - origin).x, 2) + pow((sphere.pos - origin).y, 2) + pow((sphere.pos - origin).z, 2));
 					float cTEST = glm::sqrt(pow(sphere.pos.x, 2) + pow(sphere.pos.y, 2) + pow(sphere.pos.z, 2));
-					//std::cout << "base :" << c << std::endl;
-					//std::cout << "test :" << cTEST << std::endl;
 					float sinValue = sin(angle);
 					float radiusCheck = sinValue * c;
 
-					//std::cout << angle << std::endl;
+					glm::vec3 intersectPlane;
+					glm::vec3 intersectSphere;
+
+					bool planehit = false;
+					bool spherehit = false;
+					bool rayBounceStop = false;
+					while (!rayBounceStop)
+					{
+						intersectPlane = rayPlaneIntersect(BasePlaneHeight, planehit);
+						if (spherehit == false) {
+							intersectSphere = raySphereIntersect(sphere, spherehit);
+						}
+						else 
+						{
+							spherehit = false;
+						}
+						if (spherehit == true && planehit == true)
+						{
+							if ((intersectPlane - origin).length() >= (intersectSphere - origin).length())
+							{
+								spherehit = true;
+							}
+							else
+							{
+								planehit = false;
+							}
+						}
+						if (spherehit == true && planehit == false)
+						{
+							reflect(glm::normalize(intersectSphere - sphere.pos), intersectSphere);
+							//std::cout << origin.z << std::endl;
+
+						}
+						else if (spherehit == false && planehit == true)
+						{
+							//std::cout << x;
+							rayBounceStop = true;
+							glm::ivec2 BaseTexCoord(int(round(intersectPlane.x * BaseTexScale)) % BaseTexture.get_width(), int(round(intersectPlane.z * BaseTexScale)) % BaseTexture.get_height());
+							//std::cout << intersectPlane.z * BaseTexScale << std::endl;
+							brightness.x = BaseTexture[BaseTexCoord.y][BaseTexCoord.x].red;
+							brightness.y = BaseTexture[BaseTexCoord.y][BaseTexCoord.x].green;
+							brightness.z = BaseTexture[BaseTexCoord.y][BaseTexCoord.x].blue;
+						}
+						else if (spherehit == false && planehit == false)
+						{
+							rayBounceStop = true;
+							HDRIhit = true;
+						}
+
+					}
+					//while (!rayBounceStop)
+					//{
+					//	float c = glm::sqrt(pow((sphere.pos - origin).x, 2) + pow((sphere.pos - origin).y, 2) + pow((sphere.pos - origin).z, 2));
+					//	float radiusCheck = sinValue * c;
+					//	if (direction.y < 0 && direction.y * BasePlaneHeight >= 0)
+					//	{
+					//		if (spherehit)
+					//		{
+					//			std::cout << "origin :" << origin.z << std::endl;
+					//		}
+
+
+					//		planehit = true;
+					//		intersectPlane.x = ((origin.y - BasePlaneHeight) / direction.y) * direction.x + origin.x;
+
+					//		intersectPlane.y = ((origin.y - BasePlaneHeight) / direction.y) * direction.y + origin.y;
+					//		intersectPlane.z = ((origin.y - BasePlaneHeight) / direction.y) * direction.z + origin.z;
+					//	}
+					//	if (radiusCheck < sphere.radius && spherehit == false)
+					//	{
+
+					//		float IntersectDistance = sqrt(pow(sphere.radius, 2) - pow(radiusCheck, 2));
+					//		float intersectDistanceTemp = sqrt(pow(c, 2) - pow(radiusCheck, 2));
+					//		glm::vec3 intersect = direction * (intersectDistanceTemp - IntersectDistance) + origin;
+					//		std::cout << intersect.z << std::endl;
+
+					//		if ((intersect - origin).z * direction.z > 0 && glm::length(intersectPlane) > glm::length(intersect))
+					//		{
+					//				reflect(glm::normalize(intersect - sphere.pos), intersect);
+					//				std::cout << "dir  :" << direction.z << std::endl;
+					//				std::cout << "inter:" << origin.z << std::endl;
+					//				spherehit = true;
+					//				planehit = false;
+					//				std::cout << "origin :" << origin.z << std::endl;
+					//				std::cout << "origin :" << glm::normalize(intersect - sphere.pos).z << std::endl;
+					//		}
+					//		else
+					//		{
+					//			spherehit = true;
+					//		}
+					//	}
+					//	else if(planehit)
+					//	{
+					//		rayBounceStop = true;
+					//		glm::ivec2 BaseTexCoord(int(round(intersectPlane.x * BaseTexScale)) % BaseTexture.get_width(),int(round(intersectPlane.z * BaseTexScale )) % BaseTexture.get_height());
+					//		std::cout << intersectPlane.z * BaseTexScale << std::endl;
+					//		brightness.x = BaseTexture[BaseTexCoord.y][BaseTexCoord.x].red;
+					//		brightness.y = BaseTexture[BaseTexCoord.y][BaseTexCoord.x].green;
+					//		brightness.z = BaseTexture[BaseTexCoord.y][BaseTexCoord.x].blue;
+					//	}
+					//	else
+					//	{
+					//		rayBounceStop = true;
+					//	}
+					//}
 
 					if (radiusCheck < sphere.radius)
 					{
-						//std::cout << "dir  :" << ray.direction.x << " " << ray.direction.y << " " << ray.direction.z << std::endl;
-						//std::cout << "origin:" << ray.origin.x << " " << ray.origin.y << " " << ray.origin.z << std::endl;
-						//std::cout << "test :" << c << std::endl;
-						//brightness.x = 255;
-						//brightness.y = 255;
-						//brightness.z = 255;
 
-						float IntersectDistance = sqrt(pow(sphere.radius, 2) - pow(radiusCheck, 2));
-						//std::cout << "before: " << ray.direction.x << " " << ray.direction.y << " " << ray.direction.z << std::endl;
-						float intersectDistanceTemp = sqrt(pow(c, 2) - pow(radiusCheck, 2));
-						glm::vec3 intersect = ray.direction * (intersectDistanceTemp - IntersectDistance) + ray.origin;
-						//std::cout << "origin:" << ray.origin.x << " " << ray.origin.y << " " << ray.origin.z << std::endl;
-						ray.origin = intersect;
-						ray.reflect(glm::normalize(intersect - sphere.pos), intersect);
-						//std::cout << glm::length((intersect - sphere.pos)) << std::endl;
-						////std::cout << "after : " << ray.direction.x << " " << ray.direction.y << " " << ray.direction.z << std::endl;
-						/*std::cout << "intersectd : " << intersectdistance << std::endl;
-						std::cout << "check      : " << radiuscheck << std::endl;
-						std::cout << "normal     : " << -glm::normalize(intersect - sphere.pos).x << " " << -glm::normalize(intersect - sphere.pos).y << " " << -glm::normalize(intersect - sphere.pos).z << std::endl;
-						std::cout << "direction  : " << ray.direction.x << " " << ray.direction.y << " " << ray.direction.z << std::endl;
-						std::cout << "intersect  : " << intersect.x << " " << intersect.y << " " << intersect.z << std::endl;
-						std::cout << "testreflect: " << glm::reflect(ray.direction, glm::normalize(intersect - sphere.pos)).x << " " << glm::reflect(ray.direction, glm::normalize(intersect - sphere.pos)).y << " " << glm::reflect(ray.direction, glm::normalize(intersect - sphere.pos)).z << std::endl;
-						std::cout << "testangle  : " << glm::angle(glm::vec3(-0.0145698, -0.39972, 0.916521), glm::vec3(-0.0356566, -0.97823, -0.204436)) << std::endl;
-						std::cout << x << std::endl;*/
 					}
 
-					glm::vec2 HDRIAngle(atan(ray.direction.x / ray.direction.z) + PI, atan(ray.direction.y / glm::length(glm::vec2(ray.direction.z, ray.direction.x))));
-
-					//if (ray.direction.y < 0 && y > 540)
-						//std::cout << y << " " << HDRIAngle.y << " " << HDRIAngle.x << std::endl;
+					glm::vec2 HDRIAngle(atan(direction.x / direction.z) + PI, atan(direction.y / glm::length(glm::vec2(direction.z, direction.x))));
 
 					glm::vec3 testvec(float(x) - image.get_width() / 2 + 0.5f, float(y) - image.get_height() / 2 + 0.5f, float(z));
 
-					//std::cout << x << " " << testvec.x << " " << testvec.z << std::endl;
-
-					if (ray.direction.z >= 0 && ray.direction.x >= 0)
+					if (direction.z >= 0 && direction.x >= 0)
 					{
-
-						//std::cout << "1  :" << ray.direction.x << " " << ray.direction.z << " " << HDRIAngle.x << std::endl;
-						//std::cout << "xy1 :" << x << " " << y << std::endl;
 					}
-					else if (ray.direction.z < 0 && ray.direction.x >= 0)
+					else if (direction.z < 0 && direction.x >= 0)
 					{
 
 						HDRIAngle.x += PI;
 						//std::cout << "2 :" << HDRIAngle.x << " " << HDRIAngle.y << std::endl;
 					}
-					else if (ray.direction.z >= 0 && ray.direction.x < 0)
+					else if (direction.z >= 0 && direction.x < 0)
 					{
-						//std::cout << "3 :" << HDRIAngle.x << " " << HDRIAngle.y << std::endl;
-						//std::cout << "xy2 :" << x << " " << y << std::endl;
 						HDRIAngle.x += 2 * PI;
 
 					}
-					else if (ray.direction.z < 0 && ray.direction.x < 0)
+					else if (direction.z < 0 && direction.x < 0)
 					{
 						HDRIAngle.x += PI;
 						//std::cout << "4 :" <<  HDRIAngle.x << " " << HDRIAngle.y << std::endl;
@@ -133,39 +199,66 @@ public:
 
 					if (HDRIAngle.x > 2 * PI)
 					{
-						//std::cout << "test" << std::endl;
 						HDRIAngle.x -= 2 * PI;
 					}
 					if (HDRIAngle.x < 0)
 					{
-						//std::cout << "test" << std::endl;
 						HDRIAngle.x += 2 * PI;
 					}
-
-
-					//glm::vec2 HDRIAngle(atan(ray.direction.x / ray.direction.z) + PI, atan(ray.direction.y / abs(ray.direction.z)) + PI);
-					//std::cout << HDRIAngle.y << std::endl;
-					//glm::ivec2 HDRIcoord((int)round((HDRI.get_width() / (2 * PI) - 0.5) * HDRIAngle.x), (int)round(((HDRI.get_height() / (2 * PI) - 0.5) * (HDRIAngle.x + 0.5 * PI))));
 					glm::ivec2 HDRIcoord((int)round((HDRI.get_width() / (2 * PI) - 0.5) * HDRIAngle.x), (int)round(((HDRI.get_height() / 2 + HDRIAngle.y * HDRI.get_height() / PI))));
 
-					//glm::ivec2 HDRIcoord((int)round((HDRI.get_width() / (2 * PI) - 0.5) * HDRIAngle.x), (int)round(HDRI.get_height() / 2 + HDRIAngle.y * HDRI.get_height() / (2 * PI)));
-					//std::cout << x << " " << HDRIAngle.x << HDRIcoord.x << " " << HDRIcoord.y << std::endl;
-					brightness.x = HDRI[HDRIcoord.y][HDRIcoord.x].red;
-					//std::cout << (int)HDRI[HDRIcoord.y][HDRIcoord.x].red << std::endl;
-					brightness.y = HDRI[HDRIcoord.y][HDRIcoord.x].green;
-					brightness.z = HDRI[HDRIcoord.y][HDRIcoord.x].blue;
+					if (planehit == false)
+					{
+						brightness.x = HDRI[HDRIcoord.y][HDRIcoord.x].red;
+						brightness.y = HDRI[HDRIcoord.y][HDRIcoord.x].green;
+						brightness.z = HDRI[HDRIcoord.y][HDRIcoord.x].blue;
+					}
+
 					count++;
 					HDRIhit = true;
+
+
+
+					image[y][x] = png::rgb_pixel(brightness.x, brightness.y, brightness.z);
 				}
-
-
-				//if(brightness > 10)
-					//std::cout << brightness << std::endl;
-				image[y][x] = png::rgb_pixel(brightness.x, brightness.y, brightness.z);
 			}
 		}
 	}
 private:
+	glm::vec3 raySphereIntersect(sphere& sphere, bool& spherehit)
+	{
+		glm::vec3 intersect = glm::vec3(0, 0, 0);
+		float angle = glm::angle(direction, glm::normalize(sphere.pos - origin));
+		float c = glm::sqrt(pow((sphere.pos - origin).x, 2) + pow((sphere.pos - origin).y, 2) + pow((sphere.pos - origin).z, 2));
+		float sinValue = sin(angle);
+		float radiusCheck = sinValue * c;
+		if (radiusCheck <= sphere.radius)
+		{
+			float IntersectDistance = sqrt(pow(sphere.radius, 2) - pow(radiusCheck, 2));
+			float intersectDistanceTemp = sqrt(pow(c, 2) - pow(radiusCheck, 2));
+			intersect = direction * (intersectDistanceTemp - IntersectDistance) + origin;
+			spherehit = true;
+			
+		}
+		return intersect;
+	}
+	glm::vec3 rayPlaneIntersect(float& BasePlaneHeight, bool& planehit)
+	{
+		glm::vec3 intersectPlane = glm::vec3(0, 0, 0);;
+		planehit = false;
+		if (direction.y < 0 && origin.y > BasePlaneHeight || direction.y > 0 && origin.y < BasePlaneHeight)
+		{
+			planehit = true;
+			intersectPlane.x = ((origin.y - BasePlaneHeight) / direction.y) * direction.x + origin.x;
+
+			intersectPlane.y = ((origin.y - BasePlaneHeight) / direction.y) * direction.y + origin.y;
+			intersectPlane.z = ((origin.y - BasePlaneHeight) / direction.y) * direction.z + origin.z;
+		}
+		return intersectPlane;
+
+
+	}
+
 
 };
 
--- source.cpp
+++ source.cpp
@@ -22,153 +22,17 @@ int main()
 
 	sphere sphere0;
 	sphere0.pos.x = 0.0f;
-	sphere0.pos.y = 0.0f;
+	sphere0.pos.y = 2.0f;
 	sphere0.pos.z = 5.0f;
 	sphere0.radius = 2.0f;
 	sphere spheres[] = {sphere0};
 	ray ray;
 
 	png::image< png::rgb_pixel > HDRI("assets/studio_country_hall_4k.png");
-
-	ray.rayTrace(spheres[0], image, Fov, HDRI);
-	//ray ray;
-	//ray.origin = glm::vec3(0.0f, 0.0f, 0.0f);
-
-	//png::image< png::rgb_pixel > hdri("assets/studio_country_hall_4k.png");
-
-	//for (png::uint_32 y = 0; y < resheight; y++) {
-
-	//	for (png::uint_32 x = 0; x < reswidth; x++) {
-	//		
-	//		ray.origin = glm::vec3(0, 0, 0);
-
-	//		glm::ivec3 brightness(0, 0, 0);
-	//		float z = focallength;
-	//		auto testvector = glm::vec3(float(x) - reswidth/2 + 0.5f , float(y) - resheight / 2 + 0.5f, float(z));
-	//		ray.direction = glm::normalize(testvector);
-	//		
-	//		glm::vec3 spherepositionnormalized = glm::normalize(sphere0.pos);
-	//		float angle = glm::angle(ray.direction, spherepositionnormalized);
-	//		//std::cout << angle << std::endl;
-	//		float c = glm::sqrt(pow(sphere0.pos.x, 2) + pow(sphere0.pos.y, 2) + pow(sphere0.pos.z, 2));
-	//		float sinvalue = sin(angle);
-	//		float radiuscheck = sinvalue * c;
-	//		//std::cout << angle << std::endl;
-
-
-	//		bool hdrihit = false;
-
-	//		int count = 0;
-
-	//		while (!hdrihit)
-	//		{
-	//			float angle = glm::angle(ray.direction, glm::normalize(sphere0.pos - ray.origin));
-	//			//std::cout << glm::reflect(glm::vec3(0.0f, 1.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f)).x << glm::reflect(glm::vec3(0.0f, 1.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f)).y << glm::reflect(glm::vec3(0.0f, 1.0f, 1.0f), glm::vec3(0.0f, 1.0f, 0.0f)).z << std::endl;
-	//			float c = glm::sqrt(pow((sphere0.pos - ray.origin).x, 2) + pow((sphere0.pos- ray.origin).y, 2) + pow((sphere0.pos - ray.origin).z, 2));
-	//			float ctest = glm::sqrt(pow(sphere0.pos.x, 2) + pow(sphere0.pos.y, 2) + pow(sphere0.pos.z, 2));
-	//			//std::cout << "base :" << c << std::endl;
-	//			//std::cout << "test :" << ctest << std::endl;
-	//			float sinvalue = sin(angle);
-	//			float radiuscheck = sinvalue * c;
-
-	//			//std::cout << angle << std::endl;
-
-	//			if (radiuscheck < sphere0.radius)
-	//			{
-	//				//std::cout << "dir  :" << ray.direction.x << " " << ray.direction.y << " " << ray.direction.z << std::endl;
-	//				//std::cout << "origin:" << ray.origin.x << " " << ray.origin.y << " " << ray.origin.z << std::endl;
-	//				//std::cout << "test :" << c << std::endl;
-	//				//brightness.x = 255;
-	//				//brightness.y = 255;
-	//				//brightness.z = 255;
-	//				
-	//				float intersectdistance = sqrt(pow(sphere0.radius, 2) - pow(radiuscheck, 2));
-	//				//std::cout << "before: " << ray.direction.x << " " << ray.direction.y << " " << ray.direction.z << std::endl;
-	//				float intersectdistancetemp = sqrt(pow(c, 2) - pow(radiuscheck, 2));
-	//				glm::vec3 intersect = ray.direction * (intersectdistancetemp - intersectdistance) + ray.origin; 
-	//				//std::cout << "origin:" << ray.origin.x << " " << ray.origin.y << " " << ray.origin.z << std::endl;
-	//				ray.origin = intersect;
-	//				ray.reflect(glm::normalize(intersect - sphere0.pos), intersect);
-	//				//std::cout << glm::length((intersect - sphere0.pos)) << std::endl;
-	//				////std::cout << "after : " << ray.direction.x << " " << ray.direction.y << " " << ray.direction.z << std::endl;
-	//				/*std::cout << "intersectd : " << intersectdistance << std::endl;
-	//				std::cout << "check      : " << radiuscheck << std::endl;
-	//				std::cout << "normal     : " << -glm::normalize(intersect - sphere0.pos).x << " " << -glm::normalize(intersect - sphere0.pos).y << " " << -glm::normalize(intersect - sphere0.pos).z << std::endl;
-	//				std::cout << "direction  : " << ray.direction.x << " " << ray.direction.y << " " << ray.direction.z << std::endl;
-	//				std::cout << "intersect  : " << intersect.x << " " << intersect.y << " " << intersect.z << std::endl;
-	//				std::cout << "testreflect: " << glm::reflect(ray.direction, glm::normalize(intersect - sphere0.pos)).x << " " << glm::reflect(ray.direction, glm::normalize(intersect - sphere0.pos)).y << " " << glm::reflect(ray.direction, glm::normalize(intersect - sphere0.pos)).z << std::endl;
-	//				std::cout << "testangle  : " << glm::angle(glm::vec3(-0.0145698, -0.39972, 0.916521), glm::vec3(-0.0356566, -0.97823, -0.204436)) << std::endl;
-	//				std::cout << x << std::endl;*/
-	//			}
-
-	//			glm::vec2 hdriangle(atan(ray.direction.x / ray.direction.z) + pi, atan(ray.direction.y / glm::length(glm::vec2(ray.direction.z, ray.direction.x))));
-
-	//			//if (ray.direction.y < 0 && y > 540)
-	//				//std::cout << y << " " << hdriangle.y << " " << hdriangle.x << std::endl;
-
-	//			glm::vec3 testvec(float(x) - reswidth / 2 + 0.5f, float(y) - resheight / 2 + 0.5f, float(z));
-	//			
-	//			//std::cout << x << " " << testvec.x << " " << testvec.z << std::endl;
-
-	//			if (ray.direction.z >= 0 && ray.direction.x >= 0)
-	//			{
-	//				
-	//				//std::cout << "1  :" << ray.direction.x << " " << ray.direction.z << " " << hdriangle.x << std::endl;
-	//				//std::cout << "xy1 :" << x << " " << y << std::endl;
-	//			}
-	//			else if (ray.direction.z < 0 && ray.direction.x >= 0)
-	//			{
-
-	//				hdriangle.x += pi;
-	//				//std::cout << "2 :" << hdriangle.x << " " << hdriangle.y << std::endl;
-	//			}
-	//			else if (ray.direction.z >= 0 && ray.direction.x < 0)
-	//			{
-	//				//std::cout << "3 :" << hdriangle.x << " " << hdriangle.y << std::endl;
-	//				//std::cout << "xy2 :" << x << " " << y << std::endl;
-	//				hdriangle.x += 2 * pi;
-
-	//			}
-	//			else if (ray.direction.z < 0 && ray.direction.x < 0)
-	//			{
-	//				hdriangle.x += pi;
-	//				//std::cout << "4 :" <<  hdriangle.x << " " << hdriangle.y << std::endl;
-	//			}
-
-
-	//			if (hdriangle.x > 2 * pi)
-	//			{
-	//				//std::cout << "test" << std::endl;
-	//				hdriangle.x -= 2 * pi;
-	//			}
-	//			if (hdriangle.x < 0)
-	//			{
-	//				//std::cout << "test" << std::endl;
-	//				hdriangle.x += 2 * pi;
-	//			}
-
-
-	//			//glm::vec2 hdriangle(atan(ray.direction.x / ray.direction.z) + pi, atan(ray.direction.y / abs(ray.direction.z)) + pi);
-	//			//std::cout << hdriangle.y << std::endl;
-	//			//glm::ivec2 hdricoord((int)round((hdri.get_width() / (2 * pi) - 0.5) * hdriangle.x), (int)round(((hdri.get_height() / (2 * pi) - 0.5) * (hdriangle.x + 0.5 * pi))));
-	//			glm::ivec2 hdricoord((int)round((hdri.get_width() / (2 * pi) - 0.5) * hdriangle.x), (int)round(((hdri.get_height() / 2 + hdriangle.y * hdri.get_height() / pi))));
-
-	//			//glm::ivec2 hdricoord((int)round((hdri.get_width() / (2 * pi) - 0.5) * hdriangle.x), (int)round(hdri.get_height() / 2 + hdriangle.y * hdri.get_height() / (2 * pi)));
-	//			//std::cout << x << " " << hdriangle.x << hdricoord.x << " " << hdricoord.y << std::endl;
-	//			brightness.x = hdri[hdricoord.y][hdricoord.x].red;
-	//			//std::cout << (int)hdri[hdricoord.y][hdricoord.x].red << std::endl;
-	//			brightness.y = hdri[hdricoord.y][hdricoord.x].green;
-	//			brightness.z = hdri[hdricoord.y][hdricoord.x].blue;
-	//			count++;
-	//			hdrihit = true;
-	//		}
-	//		
-
-	//		//if(brightness > 10)
-	//			//std::cout << brightness << std::endl;
-	//		image[y][x] = png::rgb_pixel(brightness.x, brightness.y, brightness.z);
-	//	}
-	//}
+	png::image< png::rgb_pixel > BaseTexture("assets/uvgrid2.png");
+	std::cout << "test";
+	ray.rayTrace(spheres[0], image, Fov, HDRI, BaseTexture);
 	image.write("test.png");
+	std::cout << "test";
 	//(x - h) ^ 2 + (y - k) ^ 2 + (z - l) - r ^ 2 = 0;
 }
